a114db2f2f471fbd53f1c0f50c2503a466974b41
Mohammed Umair <mohammedumair368@gmail.com>
2026-02-14 12:54:51 +0530

    fix: linter issue of import

--- healthcare/healthcare/doctype/service_request/service_request.py
+++ healthcare/healthcare/doctype/service_request/service_request.py
@@ -7,8 +7,7 @@ import json
 import frappe
 from frappe import _
 from frappe.model.mapper import get_mapped_doc
-from frappe.utils import now_datetime
-from frappe.utils import nowdate  
+from frappe.utils import now_datetime, nowdate 
 
 from healthcare.controllers.service_request_controller import ServiceRequestController
 from healthcare.healthcare.doctype.observation.observation import add_observation
@@ -479,6 +478,9 @@ def make_sales_invoice(source_name, target_doc=None):
 
 	sr = frappe.get_doc("Service Request", service_request)
 
+	if sr.billing_status == "Invoiced":
+		frappe.throw(_("Service Request {0} is already invoiced").format(sr.name))
+		
 	if not sr.template_dn:
 		frappe.throw(_("Template not selected"))
 
