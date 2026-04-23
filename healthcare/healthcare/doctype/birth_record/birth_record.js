7ab04bc317fdbd97f2e82f350a5551011a9e3e38
Mohammed Umair <mohammedumair368@gmail.com>
2026-04-23 17:42:54 +0530

    feat(healthcare): add Birth Record DocType with newborn child table

---
+++ healthcare/healthcare/doctype/birth_record/birth_record.js
@@ -0,0 +1,37 @@
+// Copyright (c) 2026, earthians Health Informatics Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Birth Record", {
+// 	refresh(frm) {
+
+// 	},
+// });
+frappe.ui.form.on('Birth Record', {
+    
+    mother_patient(frm) {
+        // Clear dependent fields when mother changes
+        frm.set_value('inpatient_record', null);
+        frm.set_value('patient_encounter', null);
+    },
+
+    onload(frm) {
+        // Filter Inpatient Record
+        frm.set_query('inpatient_record', function() {
+            return {
+                filters: {
+                    patient: frm.doc.mother_patient
+                }
+            };
+        });
+
+        // Filter Patient Encounter
+        frm.set_query('patient_encounter', function() {
+            return {
+                filters: {
+                    patient: frm.doc.mother_patient
+                }
+            };
+        });
+    }
+
+});
