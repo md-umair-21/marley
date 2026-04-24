507961304ce47283535c0a3c94b1425da3b9cb78
Mohammed Umair <mohammedumair368@gmail.com>
2026-04-24 10:54:30 +0530

    fix: linter issues

--- healthcare/healthcare/doctype/death_record/death_record.js
+++ healthcare/healthcare/doctype/death_record/death_record.js
@@ -7,30 +7,29 @@
 // 	},
 // });
 frappe.ui.form.on("Death Record", {
-    patient(frm) {
-        // Clear dependent fields when patient changes
-        frm.set_value("inpatient_record", null);
-        frm.set_value("patient_encounter", null);
-    },
+	patient(frm) {
+		// Clear dependent fields when patient changes
+		frm.set_value("inpatient_record", null);
+		frm.set_value("patient_encounter", null);
+	},
 
-    onload(frm) {
-        // Filter Inpatient Record
-        frm.set_query("inpatient_record", function() {
-            return {
-                filters: {
-                    "patient": frm.doc.patient
-                }
-            };
-        });
+	onload(frm) {
+		// Filter Inpatient Record
+		frm.set_query("inpatient_record", function () {
+			return {
+				filters: {
+					patient: frm.doc.patient,
+				},
+			};
+		});
 
-        // Filter Patient Encounter
-        frm.set_query("patient_encounter", function() {
-            return {
-                filters: {
-                    "patient": frm.doc.patient
-                }
-            };
-        });
-    }
-
+		// Filter Patient Encounter
+		frm.set_query("patient_encounter", function () {
+			return {
+				filters: {
+					patient: frm.doc.patient,
+				},
+			};
+		});
+	},
 });
